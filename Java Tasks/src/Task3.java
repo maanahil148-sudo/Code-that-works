7d80bd19a8dd9a25b0091155f0a7b338ca5eb6dd
maanahil148-sudo <maanahil148@gmail.com>
2025-09-28 11:09:50 +0500

    tasks

---
+++ Java Tasks/src/Task3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+    String Color;
+    System.out.println("Enter Any Traffic Light Colour");
+    Color= Scanner.nextLine();
+    Scanner.close();
+    if (Color.equals("red")) {
+        System.out.println("Stop");
+        }
+    else if(Color.equals("yellow")) {
+         System.out.println("Slow Down");
+
+     }
+     else if(Color.equals("green")) {
+         System.out.println("Go");
+
+    }
+    else{
+          System.out.println("Invalid color. Please enter red, yellow, or green"); 
+    }
+    
+}
+}
